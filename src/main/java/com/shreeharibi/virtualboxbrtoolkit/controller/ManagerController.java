b845be9c2e70cb1b39956e30e0b1548a889bda38
Shree Hari Bittugondanahalli Indra Kumar <shree_hari.bittugondanahalli_indra_kumar@mailbox.tu-dresden.de>
2021-08-04 22:15:52 +0200

    Implement get virtual machines api

--- src/main/java/com/shreeharibi/virtualboxbrtoolkit/controller/ManagerController.java
+++ src/main/java/com/shreeharibi/virtualboxbrtoolkit/controller/ManagerController.java
@@ -4,7 +4,10 @@ import com.shreeharibi.virtualboxbrtoolkit.services.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/manager")
@@ -23,6 +26,16 @@ public class ManagerController {
         }
         catch (IllegalStateException e) {
             return e.getMessage();
+        }
+    }
+
+    @GetMapping("vm")
+    @ResponseBody
+    public List<String> getVirtualMachines() {
+        try {
+            return managerService.getVirtualMachines();
+        } catch (IllegalStateException e) {
+            return List.of(e.getMessage());
         }
     }
 }
